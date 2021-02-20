32a26308935e7fffcd58fbc87b35eb33751d2431
dafinysouza <22901057+dafinysouza@users.noreply.github.com>
2021-02-20 14:25:41 -0300

    Módulo 1: Segunda aula

---
+++ src/components/commons/Menu/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Logo } from '../../theme/Logo';
+import { MenuWrapper } from './styles/MenuWrapper';
+
+export default function Menu() {
+    const links = [
+        {
+            text: 'Home',
+            url: '/'
+        },
+        {
+            text: 'Perguntas frequentes',
+            url: '/'
+        },
+        {
+            text: 'Sobre',
+            url: '/sobre'
+        }
+    ]
+    return (
+        <MenuWrapper>
+            <MenuWrapper.LeftSide>
+                <Logo />
+            </MenuWrapper.LeftSide>
+            <MenuWrapper.CentralSide>
+                {links.map(function(link) {
+                    return (
+                        <li>
+                            <a href={link.url}>
+                                {link.text}
+                            </a>
+                        </li>
+                    )
+                })}
+            </MenuWrapper.CentralSide>
+            <MenuWrapper.RightSide>
+                <button>
+                    Entrar
+                </button>
+                <button>
+                    Cadastrar
+                </button>
+            </MenuWrapper.RightSide>
+        </MenuWrapper>
+    )
+}
