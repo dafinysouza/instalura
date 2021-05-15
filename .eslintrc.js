8d5a920762417117fb8a954ab9d550ee07f7e75b
dafinysouza <22901057+dafinysouza@users.noreply.github.com>
2021-05-14 23:25:17 -0300

    test: teste de componentes
    Testando componentes com React Testing Library

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
     'cypress/globals': true,
+    'jest/globals': true,
   },
   extends: [
     'plugin:cypress/recommended',
@@ -21,6 +22,19 @@ module.exports = {
   ],
   rules: {
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
-    'import/prefer-default-export': 0,
+    'import/prefer-default-export': 'off',
   },
+  overrides: [
+    {
+      files: [
+        '**/*.test.js',
+      ],
+      plugins: ['jest'],
+      env: {
+        jest: true,
+      },
+      // eslint-disable-next-line global-require, import/no-extraneous-dependencies
+      ...require('eslint-plugin-jest').configs.recommended,
+    },
+  ],
 };
