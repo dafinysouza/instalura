be3709f65f48ace8b0558228ee53ad784572141c
dafinysouza <22901057+dafinysouza@users.noreply.github.com>
2021-05-15 15:00:06 -0300

    feat: finalização do formulário
    Finalização do formulário e testes do mesmo

--- src/infra/hooks/forms/useForm/index.js
+++ src/infra/hooks/forms/useForm/index.js
@@ -1,7 +1,38 @@
 import React from 'react';
 
-export function useForm({ initialValues, onSubmit }) {
+export function useForm({
+  initialValues,
+  onSubmit,
+  validateSchema,
+}) {
   const [values, setValues] = React.useState(initialValues);
+
+  const [isFormDisabled, setIsFormDisabled] = React.useState(true);
+  const [errors, setErrors] = React.useState({});
+  const [touched, setTouchedFields] = React.useState({});
+
+  async function validateValues(currentValues) {
+    try {
+      await validateSchema(currentValues);
+      setErrors({});
+      setIsFormDisabled(false);
+    } catch (err) {
+      const formatedErrors = err.inner.reduce((errorObjectAcc, currentError) => {
+        const fieldName = currentError.path;
+        const errorMessage = currentError.message;
+        return {
+          ...errorObjectAcc,
+          [fieldName]: errorMessage,
+        };
+      }, {});
+      setErrors(formatedErrors);
+      setIsFormDisabled(true);
+    }
+  }
+
+  React.useEffect(() => {
+    validateValues(values);
+  }, [values]);
 
   return {
     values,
@@ -17,6 +48,19 @@ export function useForm({ initialValues, onSubmit }) {
         ...currentValues,
         [fieldName]: value,
       }));
+    },
+    // Validação do Form
+    isFormDisabled,
+    setIsFormDisabled,
+    errors,
+    touched,
+    handleBlur(event) {
+      const fieldName = event.target.getAttribute('name');
+
+      setTouchedFields({
+        ...touched,
+        [fieldName]: true, // usuario: true, senha: true ...
+      });
     },
   };
 }
